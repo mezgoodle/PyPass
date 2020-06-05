10c97b6544b1103ccfa65e8e45f1c507a0a5712c
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-05 19:16:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 require 'db.php';
 ?>
-<a href="/login.php">Login</a><br>
-<a href="/signup.php">Sign up</a>
+
+<?php if (isset($_SESSION['logged_user'])) : ?>
+    logged!
+    <hr>
+    <a href="/logout.php">Logout</a>
+<?php else : ?>
+    <a href="/login.php">Login</a><br>
+    <a href="/signup.php">Sign up</a>
+<?php endif; ?>
