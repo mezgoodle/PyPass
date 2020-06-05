d69e6d74b376cce9746dd007af816024409089c2
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-05 18:34:04 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,19 +1,40 @@
 <?php
     require "db.php";
+
+    $data = $_POST;
+    if (isset($data['do_signup'])) {
+        # register here
+        $errors = array();
+        if (trim($data['login']) == '') {
+            $errors[] = 'Enter name!';
+        }
+        if ($data['password'] == '') {
+            $errors[] = 'Enter password!';
+        }
+        if ($data['password_2'] != $data['password']) {
+            $errors[] = 'Second password is not match!';
+        }
+
+        if (empty($errors)) {
+            # all is good
+        } else {
+            echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
+        }
+    }
 ?>
 
 <form action="/signup.php" method="post">
     <p>
         <label for="username">Your name</label>
-        <input type="text" name="login" id="username">
+        <input type="text" name="login" id="username" value="<?php echo @$data['login']; ?>">
     </p>
     <p>
         <label for="password">Your password</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="password" value="<?php echo @$data['password']; ?>">
     </p>
     <p>
         <label for="password_2">Your password again</label>
-        <input type="password" name="password_2" id="password_2">
+        <input type="password" name="password_2" id="password_2" value="<?php echo @$data['password_2']; ?>">
     </p>
-    <p><button type="submit">Sign up</button></p>
+    <p><button type="submit" name="do_signup">Sign up</button></p>
 </form>
