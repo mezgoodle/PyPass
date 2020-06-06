f22db519ded0fc56a6f205ccb2d03fd18182a489
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-06 21:15:40 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -6,14 +6,14 @@ const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
+const clipboard = document.getElementById('clipboard');
 
 const randomFunc = {
-    'lower': getRandomLower,
-    'upper': getRandomUpper,
-    'number': getRandomNumber,
-    'symbol': getRandomSymbol,
-};
+	'lower': getRandomLower,
+	'upper': getRandomUpper,
+	'number': getRandomNumber,
+	'symbol': getRandomSymbol
+}
 
 // Generator functions
 
@@ -21,9 +21,8 @@ const getRandomLower = () => String.fromCharCode(Math.floor(Math.random() * 26) 
 
 const getRandomUpper = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
-const getRandomNumber = () => String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+const getRandomNumber = () => (+String.fromCharCode(Math.floor(Math.random() * 10) + 48));
 
 const getRandomSymbol = () => {
-    const symbols = '!@#$%^&*(){}[]=<>/,.';
-    return symbols[Math.floor(Math.random() * symbols.length)];
+	const symbols = '!@#$%^&*(){}[]=<>/,.';
 }
