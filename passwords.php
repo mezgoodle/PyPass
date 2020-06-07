76c24619c4d0e8e88c26ada77159c5730e9f6877
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-07 21:56:10 +0300

    Update passwords.php

--- passwords.php
+++ passwords.php
@@ -1,20 +1,66 @@
 <?php 
-    $names = array("Alfreds Futterkiste", "Berglunds snabbkop", "Island Trading", "Koniglich Essen");
-    $countries = array("Germany", "Sweden", "UK", "Germany");
-    if (!isset($_SESSION['logged_user'])) { ?>
+    require './db.php';
+
+    $data = $_POST;
+    if (isset($data['do_save'])) {
+        # save here
+        $errors = array();
+        if (trim($data['passname']) == '') {
+            $errors[] = 'Enter passwords name!';
+        }
+        if ($data['password'] == '') {
+            $errors[] = 'Enter password!';
+        }
+
+        if (empty($errors)) {
+            # all is good
+            $password = R::dispense('passwords');
+            $password->name = $data['passname'];
+            $password->username = $_SESSION['logged_user']->login;
+            $password->password = $data['password'];
+            R::store($password);
+            echo '<div style="color: green;">Your password has been saved!</div><hr>';
+        } else {
+            echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
+        }
+    }
+    if (isset($_SESSION['logged_user'])) {
+        $passwords = R::find('passwords', 'username = ?', array($_SESSION['logged_user']->login));
+        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
 
 <table id="myTable">
   <tr class="header">
-    <th style="width:60%;">Name</th>
-    <th style="width:40%;">Country</th>
+    <th style="width:60%;">Password name</th>
+    <th style="width:40%;">Password</th>
   </tr>
-  <?php for ($i=0; $i < count($names); $i++) { ?>
-    <tr>
-      <td><?php echo $names[$i]; ?></td>
-      <td><?php echo $countries[$i]; ?></td>
-    </tr>
- <?php } ?>
+  <?php 
+    foreach ($passwords as $password) { ?>
+        <td><?php echo $password->name; ?></td>
+      <td><?php echo $password->password; ?></td>
+    <?php } ?>
 </table>
+<form action="/passwords.php" method="post">
+    <p>
+        <label for="passname">Password name</label>
+        <input type="text" name="passname" id="passname" value="<?php echo @$data['passname']; ?>">
+    </p>
+    <p>
+        <label for="password">Your password</label>
+        <input type="password" name="password" id="password" value="<?php echo @$data['password']; ?>">
+    </p>
+    <p><button type="submit" name="do_save">Save</button></p>
+</form>
+<?php require_once 'generator.html';?>
+</body>
+</html>
 <?php } else { header('Location: /login.php'); } ?>
 <script src="./libs/search.js"></script>
