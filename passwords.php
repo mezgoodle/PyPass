11efbf02e99d252ec58b57efbb5be1cdef5267e2
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-08 10:54:32 +0300

    Update passwords template

--- passwords.php
+++ passwords.php
@@ -19,48 +19,54 @@
             $password->username = $_SESSION['logged_user']->login;
             $password->password = $data['password'];
             R::store($password);
-            echo '<div style="color: green;">Your password has been saved!</div><hr>';
+            echo '<div style="color: green;">Your password has been saved!</div>';
         } else {
-            echo '<div style="color: red;">'.array_shift($errors).'</div><hr>';
+            echo '<div style="color: red;">'.array_shift($errors).'</div>';
         }
     }
     if (isset($_SESSION['logged_user'])) {
         $passwords = R::find('passwords', 'username = ?', array($_SESSION['logged_user']->login));
         ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
-
-<table id="myTable">
-  <tr class="header">
-    <th style="width:60%;">Password name</th>
-    <th style="width:40%;">Password</th>
-  </tr>
-  <?php 
-    foreach ($passwords as $password) { ?>
-        <td><?php echo $password->name; ?></td>
-      <td><?php echo $password->password; ?></td>
-    <?php } ?>
-</table>
-<form action="/passwords.php" method="post">
-    <p>
-        <label for="passname">Password name</label>
-        <input type="text" name="passname" id="passname" value="<?php echo @$data['passname']; ?>">
-    </p>
-    <p>
-        <label for="password">Your password</label>
-        <input type="password" name="password" id="password" value="<?php echo @$data['password']; ?>">
-    </p>
-    <p><button type="submit" name="do_save">Save</button></p>
-</form>
-<a class="btn btn-primary btn-lg" href="/generator.html" target="_blank" role="button">Password generator</a>
-</body>
-</html>
-<?php } else { header('Location: /login.php'); } ?>
+<?php require_once './templates/header.php'; ?>
+<div class="container-fluid">
+    <input type="text" id="myInput" class="form-control" onkeyup="myFunction()" placeholder="Search for passwords by name">
+    <table class="table" id="myTable">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Password name</th>
+        <th scope="col">Password</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        for ($i=1; $i <= count($passwords); $i++) { ?>
+        <tr>
+        <th scope="row"><?php echo $i; ?></th>
+        <td><?php echo $passwords[$i]->name; ?></td>
+        <td><?php echo $passwords[$i]->password; ?></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+    </table>
+</div>
+<hr>
+<div class="container">
+    <form action="/passwords.php" method="post">
+        <div class="form-group">
+            <label for="passname">Password name</label>
+            <input type="text" class="form-control" id="passname" name="passname" value="<?php echo @$data['passname']; ?>">
+        </div>
+        <div class="form-group">
+            <label for="password">Your password</label>
+            <input type="password" class="form-control" id="password" name="password" value="<?php echo @$data['password']; ?>">
+        </div>
+        <button type="submit" name="do_save" class="btn btn-primary">Save</button>
+    </form>
+</div>
+<div class="container text-center">
+<a class="btn btn-secondary" href="/generator.html" target="_blank" role="button">Password generator</a>
+</div>
 <script src="./libs/search.js"></script>
+<?php require_once './templates/footer.php'; ?>
+<?php } else { header('Location: /login.php'); } ?>
