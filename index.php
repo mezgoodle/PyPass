3663f600f15fdda9c8da068fe42f8ae8a87ee631
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-08 09:44:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,16 @@ require_once './templates/header.php';
 ?>
 
 <?php if (isset($_SESSION['logged_user'])) : ?>
-    logged!<br>
-    Hello, <?php echo $_SESSION['logged_user']->login; ?>
+<div class="text-center">
+    <h2 class="text-center">Hello, <?php echo $_SESSION['logged_user']->login; ?></h2>
     <hr>
-    <a href="/logout.php">Logout</a>
+    <a class="btn btn-primary btn-lg" href="/logout.php" role="button">Logout</a>
+</div>
 <?php else : ?>
-    <a href="/login.php">Login</a><br>
-    <a href="/signup.php">Sign up</a>
+<div class="container-fluid text-center">
+    <a class="btn btn-primary btn-lg" href="/login.php" role="button">Login</a>
+    <a class="btn btn-primary btn-lg" href="/signup.php" role="button">Sign up</a>
+</div>
 <?php 
     endif; 
     require_once './templates/footer.php';
